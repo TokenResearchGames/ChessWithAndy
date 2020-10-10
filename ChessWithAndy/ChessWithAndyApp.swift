6503e9f80737f5b0cf08d6ac90d42365fb645515
Andrew Pool <andrewpooljob@gmail.com>
2020-10-09 19:40:28 -0600

    Initial Commit

---
+++ ChessWithAndy/ChessWithAndyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessWithAndyApp.swift
+//  ChessWithAndy
+//
+//  Created by Andrew Pool on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct ChessWithAndyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
